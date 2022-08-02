b25ae8d450e3c2e19b3878111fad23ea8b2d0b63
varga492 <varga492@gmail.com>
2022-08-02 14:00:45 +0200

    2:33

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,38 @@
-console.log("hello")
 
-//Scopes//
+let formComponent = function () {
+    return`
+<form>
+    <input id="nameInput" type="text" placeholder="Enter your name...."/>
 
-let globalVariable = `globálisan elérhető`;
+</form>`
+};
 
-console.log(globalVariable)
+let displayComponent = function () {
+    return `
+          <p id="name-display">Your Name</p>
+      ;`
+  };
 
-function logVariable(){
-    
-    let funcVariable = `Beyoncé`
-    let globalVariable = `metallica`
-    console.log(globalVariable)
-    function changeVal(){
-        let globalVariable =`tre`
-        console.log(globalVariable)
-    }
-   changeVal()
+  let contentComponent = function (first, second) {
+    return `
+          <section class="first">${first}</section>
+          <section class="second">${second}</section>
+      ;`
+  };
+
+
+function loadEvent() {
+    console.log('page is loaded');
+    let root= document.getElementById(`root`);
+    root.insertAdjacentHTML("beforeend",formComponent)
+    let inputElement = document.querySelector(`input[type="text"]`)
+    inputElement.addEventListener("input",function (event){
+        let value = event.target.value;
+        console.log(value)
+    })
 }
 
-logVariable();
 
-//console.log(funcVariable)
 
-/* Block scope: {} köze írjuk a kódunkat pl 
-{
-    function xy(){
-        function kódja
-    }
-    xy()
-}*/
+
+window.addEventListener("load", loadEvent);
